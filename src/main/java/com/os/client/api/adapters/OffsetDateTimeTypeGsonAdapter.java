6040836de9240ddebf5ac9e380662345ea0c0d68
mschoenberg <matthew.schoenberg@equilend.com>
2026-01-02 13:06:24 -0500

    patched bug with offsetdatetime serializing

--- src/main/java/com/os/client/api/adapters/OffsetDateTimeTypeGsonAdapter.java
+++ src/main/java/com/os/client/api/adapters/OffsetDateTimeTypeGsonAdapter.java
@@ -16,31 +16,33 @@ import com.google.gson.JsonSerializer;
 
 public class OffsetDateTimeTypeGsonAdapter implements JsonSerializer<OffsetDateTime>, JsonDeserializer<OffsetDateTime> {
 
-	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+			.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
 	@Override
-	public JsonElement serialize(final OffsetDateTime date, final Type typeOfSrc, final JsonSerializationContext context) {
-		return new JsonPrimitive(date.format(DATE_TIME_FORMATTER));
+	public JsonElement serialize(final OffsetDateTime date, final Type typeOfSrc,
+			final JsonSerializationContext context) {
+		return new JsonPrimitive(date.format(DATE_TIME_FORMATTER) + "Z");
 	}
 
 	@Override
-	public OffsetDateTime deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context)
-			throws JsonParseException {
-		
+	public OffsetDateTime deserialize(final JsonElement json, final Type typeOfT,
+			final JsonDeserializationContext context) throws JsonParseException {
+
 		String dateAsString = json.getAsString();
-		
+
 		if (dateAsString == null) {
 			throw new JsonParseException("OffsetDateTime argument is null.");
 		}
-		
-		int milliSize = dateAsString.substring(dateAsString.indexOf(".")+1).length();
-		
-		for (int i = milliSize; i<3; i++) {
+
+		int milliSize = dateAsString.substring(dateAsString.indexOf(".") + 1).length();
+
+		for (int i = milliSize; i < 3; i++) {
 			dateAsString = dateAsString + "0";
 		}
-		
+
 		LocalDateTime dateTime = LocalDateTime.parse(dateAsString, DATE_TIME_FORMATTER);
-		
+
 		return OffsetDateTime.of(dateTime, ZoneOffset.UTC);
 
 	}
